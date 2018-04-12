daf81a76cbb8ed8e9dc2be64fce7f178a8f7b785
maksim-dudov <maksim.dudov@gmail.com>
2018-04-12 14:26:20 +0300

    dummy crud

---
+++ protected/views/page/_view.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this PageController */
+/* @var $data Page */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('uid')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->uid), array('view', 'id'=>$data->uid)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
+	<?php echo CHtml::encode($data->title); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('content')); ?>:</b>
+	<?php echo CHtml::encode($data->content); ?>
+	<br />
+
+
+</div>
---
+++ protected/views/page/view.php
@@ -0,0 +1,28 @@
+<?php
+/* @var $this PageController */
+/* @var $model Page */
+
+$this->breadcrumbs=array(
+	'Pages'=>array('index'),
+	$model->title,
+);
+
+$this->menu=array(
+	array('label'=>'List Page', 'url'=>array('index')),
+	array('label'=>'Create Page', 'url'=>array('create')),
+	array('label'=>'Update Page', 'url'=>array('update', 'id'=>$model->uid)),
+	array('label'=>'Delete Page', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->uid),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manage Page', 'url'=>array('admin')),
+);
+?>
+
+<h1>View Page #<?php echo $model->uid; ?></h1>
+
+<?php $this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		'uid',
+		'title',
+		'content',
+	),
+)); ?>
