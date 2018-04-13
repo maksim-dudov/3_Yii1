e8a5002ef124b0b12d5a4095036e6ba7afa2b4a4
maksim-dudov <maksim.dudov@gmail.com>
2018-04-13 17:10:07 +0300

    default project description changed

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,19 +2,13 @@
 /* @var $this SiteController */
 
 $this->pageTitle=Yii::app()->name;
+
+$this->menu=array(
+	array('label'=>'Create Hotel', 'url'=>array('create')),
+	array('label'=>'Manage Hotel', 'url'=>array('admin')),
+);
 ?>
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<p>Это тестовый проект для сравнительного анализа быстродействия работы с БД инструментов настоящего фреймворка.</p>
