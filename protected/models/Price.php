2316cb4b859647a68fe86dbf60b780b9bf104501
maksim-dudov <maksim.dudov@gmail.com>
2018-05-18 13:00:16 +0300

    create and delete

--- protected/models/Price.php
+++ protected/models/Price.php
@@ -7,6 +7,7 @@
  * @property integer $uid
  * @property integer $block_uid
  * @property integer $rate_uid
+ * @property string $date
  * @property integer $value
  */
 class Price extends CActiveRecord
@@ -29,9 +30,10 @@ class Price extends CActiveRecord
 		return array(
 			array('uid', 'required'),
 			array('uid, block_uid, rate_uid, value', 'numerical', 'integerOnly'=>true),
+			array('date', 'safe'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('uid, block_uid, rate_uid, value', 'safe', 'on'=>'search'),
+			array('uid, block_uid, rate_uid, date, value', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -57,6 +59,7 @@ class Price extends CActiveRecord
 			'uid' => 'Uid',
 			'block_uid' => 'Block Uid',
 			'rate_uid' => 'Rate Uid',
+			'date' => 'Date',
 			'value' => 'Value',
 		);
 	}
@@ -82,6 +85,7 @@ class Price extends CActiveRecord
 		$criteria->compare('uid',$this->uid);
 		$criteria->compare('block_uid',$this->block_uid);
 		$criteria->compare('rate_uid',$this->rate_uid);
+		$criteria->compare('date',$this->date,true);
 		$criteria->compare('value',$this->value);
 
 		return new CActiveDataProvider($this, array(
@@ -98,5 +102,53 @@ class Price extends CActiveRecord
 	public static function model($className=__CLASS__)
 	{
 		return parent::model($className);
+	}
+
+	/**
+	 * Сохраняет цены по прямому запросу к БД
+	 * @param $block_uid идентификатор категории
+	 * @param $rate_uid идентификатор тарифа
+	 * @param $date дата для цены
+	 * @param $value цена (целое значение)
+	 * @return результат создания новой строки в таблице
+	 */
+	protected function savePriceByQuery($block_uid, $rate_uid, $date, $value)
+	{
+		return Yii::app()->db->createCommand()
+			->insert('season', array(
+				'block_uid' => 	$block_uid,
+				'rate_uid' => 	$rate_uid,
+				'date' => 		$date,
+				'value' => 		$value,
+			));
+	}
+
+
+	/**
+	 * Сохраняет цены через фреймворк
+	 * @param $block_uid идентификатор категории
+	 * @param $rate_uid идентификатор тарифа
+	 * @param $date дата для цены
+	 * @param $value цена (целое значение)
+	 * @return результат сохранения
+	 */
+	protected function savePriceBySave($block_uid, $rate_uid, $date, $value)
+	{
+		$newPrice = new Price();
+		$newPrice->block_uid = $block_uid;
+		$newPrice->rate_uid = $rate_uid;
+		$newPrice->date = $date;
+		$newPrice->value = $value;
+		return $newPrice->save();
+	}
+
+	/**
+	 * Метод удаляет все цены
+	 * @return void
+	 */
+	public function dropAllPrices()
+	{
+		return Yii::app()->db->createCommand()
+			->delete('block');
 	}
 }
