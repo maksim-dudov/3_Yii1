95f08f9a9c6a5f3568449ffcb3b77fa771ed1194
maksim-dudov <maksim.dudov@gmail.com>
2018-05-18 16:48:27 +0300

    common links of init data moved out to separated file for partial rendering

---
+++ protected/views/site/_init_links.php
@@ -0,0 +1,16 @@
+<?php
+echo CHtml::link('Создать все отели',array('site/createAllHotels'));
+echo '<br /><br />';
+echo CHtml::link('Создать случайный отель',array('site/createRandomHotel'));
+echo '<br /><br />';
+echo CHtml::link('Создать сезоны в отелях',array('site/createAllSeasons'));
+echo '<br /><br />';
+echo CHtml::link('Создать тарифы в сезонах',array('site/createAllRates'));
+echo '<br /><br />';
+echo '<br /><br />';
+echo CHtml::link('Удалить все отели',array('site/dropAllHotels'));
+echo '<br /><br />';
+echo CHtml::link('Удалить все сезоны',array('site/dropAllSeasons'));
+echo '<br /><br />';
+echo CHtml::link('Удалить все тарифы',array('site/dropAllRates'));
+?>
--- protected/views/site/init_by_orm.php
+++ protected/views/site/init_by_orm.php
@@ -35,21 +35,5 @@ $echo .= 'del - ' . (isset($del_time)?sprintf("%01.2f",$del_time):'na') . '<br /
 $echo .= '<br /><br />';
 
 echo $echo;
-?>
-
-<?php
-echo CHtml::link('Создать все отели',array('site/createAllHotels'));
-echo '<br /><br />';
-echo CHtml::link('Создать случайный отель',array('site/createRandomHotel'));
-echo '<br /><br />';
-echo CHtml::link('Создать сезоны в отелях',array('site/createAllSeasons'));
-echo '<br /><br />';
-echo CHtml::link('Создать тарифы в сезонах',array('site/createAllRates'));
-echo '<br /><br />';
-echo '<br /><br />';
-echo CHtml::link('Удалить все отели',array('site/dropAllHotels'));
-echo '<br /><br />';
-echo CHtml::link('Удалить все сезоны',array('site/dropAllSeasons'));
-echo '<br /><br />';
-echo CHtml::link('Удалить все тарифы',array('site/dropAllRates'));
+$this->renderPartial('_init_links');
 ?>
